1f1cff6c56c31d155d7cbd1e722f2abbf860fefe
Daniel Levi <163361305+odaniellevi@users.noreply.github.com>
2025-05-27 16:21:10 +0000

    bulls and cows finalizado

--- bulls-and-cows/app/page.js
+++ bulls-and-cows/app/page.js
@@ -1,7 +1,9 @@
+'use client';
+
 import { useState, useEffect } from "react";
 
 export default function JogoDaSenha() {
-  const [numeroSecreto, setNumeroSecreto] = useState(gerarNumeroSecreto());
+  const [numeroSecreto, setNumeroSecreto] = useState("");
   const [palpite, setPalpite] = useState("");
   const [tentativas, setTentativas] = useState(0);
   const [resultados, setResultados] = useState([]);
@@ -17,10 +19,14 @@ export default function JogoDaSenha() {
     return numero;
   }
 
+  useEffect(() => {
+    setNumeroSecreto(gerarNumeroSecreto());
+  }, []);
+
   const verificarTentativa = () => {
     if (palpite.length !== 4 || new Set(palpite).size !== 4) {
       setResultados((prev) => [
-        "Por favor, insira um número com 4 dígitos únicos",
+        "Por favor insira um número com 4 dígitos únicos",
         ...prev
       ]);
       return;
@@ -61,41 +67,41 @@ export default function JogoDaSenha() {
   };
 
   return (
-    <div className = "flex flex-col items-center justify-center min-h-screen bg-indigo-950 text-white p-4">
-      <div className = "flex flex-col items-center justify-center bg-indigo-950 rounded-[50px] w-80 h-[30rem] gap-4">
-        <h1 className = "text-green-500 text-center text-2xl font-bold">JOGO DA SENHA</h1>
-        <p className = "text-lg">Digite um número de 4 dígitos</p>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-[#18181b] text-white p-4">
+      <div className="flex flex-col items-center justify-center bg-[#18181b] rounded-[50px] w-80 h-[30rem] gap-4">
+        <h1 className="text-red-500 text-center text-2xl font-bold">JOGO DA SENHA</h1>
+        <p className="text-lg">Digite um número de 4 dígitos</p>
         <input
           type="text"
           placeholder="* * * *"
           value={palpite}
           onChange={(e) => setPalpite(e.target.value)}
-          className = "rounded-full text-center w-40 h-8 text-black placeholder-black"
+          className="rounded-full text-center w-40 h-8 text-white placeholder-white-700"
         />
         <button
-          onClick = {verificarTentativa}
-          className= "flex items-center justify-center w-40 h-12 bg-green-600 text-white rounded-full hover:bg-green-700"
+          onClick={verificarTentativa}
+          className="flex items-center justify-center w-40 h-12 bg-green-600 text-white rounded-full hover:bg-green-700"
         >
           VERIFICAR
         </button>
         <button
           onClick={reiniciarJogo}
-          className= "flex items-center justify-center w-40 h-12 bg-red-600 text-white rounded-full hover:bg-red-700"
+          className="flex items-center justify-center w-40 h-12 bg-red-600 text-white rounded-full hover:bg-red-700"
         >
           REINICIAR
         </button>
         <button
           onClick={mostrarCombinacao}
-          className = "flex items-center justify-center w-40 h-12 bg-green-600 text-white rounded-full hover:bg-green-700"
+          className="flex items-center justify-center w-40 h-12 bg-green-600 text-white rounded-full hover:bg-green-700"
         >
           MOSTRAR COMBINAÇÃO
         </button>
-        <div className = "text-center overflow-y-auto w-72 max-h-40 border border-white p-2 box-border">
+        <div className="text-center overflow-y-auto w-72 max-h-40 border border-white p-2 box-border">
           {resultados.map((res, idx) => (
-            <p key = {idx}>{res}</p>
+            <p key={idx}>{res}</p>
           ))}
         </div>
       </div>
     </div>
-  )
+  );
 }
